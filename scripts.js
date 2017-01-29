e58603e4a51ea4ac43ecc7ea365b56d4bec475f8
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2017-01-29 21:42:01 +0800

    udpate example

--- scripts.js
+++ scripts.js
@@ -3,17 +3,86 @@
 
 	var app = angular.module('app', ['ngBootstrapDropdown']);
 
-	app.controller('mainController', ['$scope', function($scope) {
-		$scope.options = {
-			"choice1": "Choice 1",
-			"choice2": "Choice 2",
-			"choice3": "Choice 3"
+	app.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+		$scope.years = {
+			"": ""
 		};
-		$scope.isLoading = false;
-		$scope.selected  = "";
+		$scope.makes = {
+			"": ""
+		};
+		$scope.trims = {
+			"": ""
+		};
+		$scope.models = {
+			"": ""
+		};
+		$scope.year                = "";
+		$scope.make                = "";
+		$scope.isLoading           = false;
+		$scope.isYearFieldDisabled = true;
+		$scope.isMakeFieldDisabled = false;
+		$scope.isModelFieldDisabled = false;
+		$scope.isTrimFieldDisabled = false;
 
-		$scope.onSelect = function(value) {
-			console.log(value);
+		$timeout(function() {
+			$scope.isYearFieldDisabled = false;
+			$scope.years = {
+				"2014": "2014",
+				"2015": "2015",
+				"2016": "2016",
+				"2017": "2017"
+			};
+		}, 100);
+
+		$scope.onSelectYear = function(value) {
+			$scope.isMakeFieldDisabled = true;
+			$scope.make = "";
+			$scope.makes = {};
+			$scope.model = "";
+			$scope.models = {};
+			$scope.trim = "";
+			$scope.trims = {};
+			$timeout(function() {
+				$scope.isMakeFieldDisabled = false;
+				$scope.makes = {
+					"Ford": "Ford",
+					"Honda": "Honda",
+					"Toyota": "Toyota",
+					"Nissan": "Nissan"
+				};
+			}, 100);
+		}
+
+		$scope.onSelectMake = function(value) {
+			$scope.isModelFieldDisabled = true;
+			$scope.model = "";
+			$scope.models = {};
+			$scope.trim = "";
+			$scope.trims = {};
+			$timeout(function() {
+				$scope.isModelFieldDisabled = false;
+				$scope.models = {
+					"Civic": "Civic",
+					"City": "City",
+					"Brezzy": "Brezzy",
+					"CRz": "CRz"
+				};
+			}, 100);
+		}
+
+		$scope.onSelectModel = function(value) {
+			$scope.isTrimFieldDisabled = true;
+			$scope.trim = "";
+			$scope.trims = {};
+			$timeout(function() {
+				$scope.isTrimFieldDisabled = false;
+				$scope.trims = {
+					"Trim 1": "Trim 1",
+					"Trim 2": "Trim 2",
+					"Trim 3": "Trim 3",
+					"Trim 4": "Trim 4"
+				};
+			}, 100);
 		}
 	}]);
 })(window, window.angular);
